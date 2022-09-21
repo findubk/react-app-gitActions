2a49cf8ab06bd1d364643fcfd16158a6581ea529
ubkempg <usama.khalil@empglabs.com>
2022-09-21 10:24:11 +0500

    deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
 function App() {
+  const [list, setlist] = useState([]);
+
+  useEffect(() => {
+    (() => {
+      fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((data) => data.json())
+        .then((data) => setlist(data))
+        .catch((e) => console.log(e));
+    })();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {list.map((el) => (
+        <div>{el.title}</div>
+      ))}
     </div>
   );
 }
